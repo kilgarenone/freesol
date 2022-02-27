0d52d557f9a36402bf102951fe47c9ca2b75653e
yeewei <oldjoy@protonmail.com>
2022-02-27 08:25:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## freesol
+# freesol
---
+++ test/modules/build.test.js
@@ -0,0 +1,169 @@
+const fs = require('fs');
+const expect = require('chai').expect;
+const nanogen = require('../../lib');
+const mockConfig = require('../mock/mock-config');
+
+describe('build', function () {
+  it('should empty output path', function () {
+    // given
+    fs.mkdirsSync(mockConfig.build.outputPath);
+    const testFile = `${mockConfig.build.outputPath}/test.txt`;
+    fs.writeFileSync(testFile, 'test');
+
+    // when
+    nanogen.build(mockConfig);
+
+    //then
+    expect(fs.existsSync(testFile)).to.be.false;
+  });
+
+  it('should copy assets folder', function () {
+    // when
+    nanogen.build(mockConfig);
+
+    // then
+    expect(fs.existsSync(`${mockConfig.build.outputPath}/asset.txt`)).to.be
+      .true;
+    expect(fs.existsSync(`${mockConfig.build.outputPath}/sub/another.txt`)).to
+      .be.true;
+  });
+
+  it('should use the default layout', function () {
+    // when
+    nanogen.build(mockConfig);
+
+    // then
+    const page = fs.readFileSync(
+      `${mockConfig.build.outputPath}/md1/index.html`,
+      'utf-8'
+    );
+    expect(page).to.have.string('default-layout-start');
+    expect(page).to.have.string('default-layout-end');
+  });
+
+  it('should use custom layout', function () {
+    // when
+    nanogen.build(mockConfig);
+
+    // then
+    const page = fs.readFileSync(
+      `${mockConfig.build.outputPath}/custom/index.html`,
+      'utf-8'
+    );
+    expect(page).to.have.string('custom-layout-start');
+    expect(page).to.have.string('custom-layout-end');
+  });
+
+  it('should generate pages from ejs', function () {
+    // when
+    nanogen.build(mockConfig);
+
+    // then
+    const page1 = fs.readFileSync(
+      `${mockConfig.build.outputPath}/ejs1/index.html`,
+      'utf-8'
+    );
+    expect(page1).to.have.string('<p>ejs-1</p>');
+
+    const page2 = fs.readFileSync(
+      `${mockConfig.build.outputPath}/ejs/ejs2/index.html`,
+      'utf-8'
+    );
+    expect(page2).to.have.string('<p>ejs-2</p>');
+  });
+
+  it('should generate pages from html', function () {
+    // when
+    nanogen.build(mockConfig);
+
+    // then
+    const page1 = fs.readFileSync(
+      `${mockConfig.build.outputPath}/html1/index.html`,
+      'utf-8'
+    );
+    expect(page1).to.have.string('<p>html-1</p>');
+
+    const page2 = fs.readFileSync(
+      `${mockConfig.build.outputPath}/html/html2/index.html`,
+      'utf-8'
+    );
+    expect(page2).to.have.string('<p>html-2</p>');
+  });
+
+  it('should not generate extra directory if filename is index', function () {
+    // when
+    nanogen.build(mockConfig);
+
+    // then
+    expect(
+      fs.existsSync(
+        `${mockConfig.build.outputPath}/with-index/index/index.html`
+      )
+    ).to.be.false;
+    expect(
+      fs.existsSync(`${mockConfig.build.outputPath}/with-index/index.html`)
+    ).to.be.true;
+  });
+
+  it('should inject site config', function () {
+    // when
+    nanogen.build(mockConfig);
+
+    // then
+    const page = fs.readFileSync(
+      `${mockConfig.build.outputPath}/with-site-data/index.html`,
+      'utf-8'
+    );
+    expect(page).to.have.string('site title on layout: test-site');
+    expect(page).to.have.string('site title on page: test-site');
+  });
+
+  it('should inject front matter', function () {
+    // when
+    nanogen.build(mockConfig);
+
+    // then
+    const page1 = fs.readFileSync(
+      `${mockConfig.build.outputPath}/with-front-matter-md/index.html`,
+      'utf-8'
+    );
+    expect(page1).to.have.string('front-matter on layout: test-md');
+
+    const page2 = fs.readFileSync(
+      `${mockConfig.build.outputPath}/with-front-matter-ejs/index.html`,
+      'utf-8'
+    );
+    expect(page2).to.have.string('front-matter on layout: test-ejs');
+    expect(page2).to.have.string('front-matter on page: test-ejs');
+  });
+
+  it('should include partial on layout', function () {
+    // when
+    nanogen.build(mockConfig);
+
+    // then
+    const page1 = fs.readFileSync(
+      `${mockConfig.build.outputPath}/ejs1/index.html`,
+      'utf-8'
+    );
+    expect(page1).to.have.string('<p>test-partial-from-layout</p>');
+  });
+
+  it('should include partial on pages', function () {
+    // when
+    nanogen.build(mockConfig);
+
+    // then
+    const page1 = fs.readFileSync(
+      `${mockConfig.build.outputPath}/ejs1/index.html`,
+      'utf-8'
+    );
+    expect(page1).to.have.string('<p>test-partial-from-page-ejs1</p>');
+
+    const page2 = fs.readFileSync(
+      `${mockConfig.build.outputPath}/ejs/ejs2/index.html`,
+      'utf-8'
+    );
+    expect(page2).to.have.string('<p>test-partial-from-page-ejs2</p>');
+  });
+});
